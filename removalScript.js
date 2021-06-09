a9b95d1dd8e82c5595575232ec0bf036d7b20653
XcodeBoi <runningzoomcat@gmail.com>
2021-06-10 00:47:46 +1000

    remove recommendations on player view

--- removalScript.js
+++ removalScript.js
@@ -1,8 +1,12 @@
 console.log("exectuing...")
+console.log(window.location.href)
 
-if(window.location.href.split("?")[0] != "watch") {
+if(window.location.href.split("?")[0].split("/")[3] != "watch") {
 	let yt = document.getElementById("primary")
 	yt.parentNode.removeChild(yt)
+} else {
+	let items = document.getElementById("items")
+	items.parentNode.removeChild(items)
 }
 
 console.log("sucess")
