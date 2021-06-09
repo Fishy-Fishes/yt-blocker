ee534d8faef00c6ae33d678778efbf8fd1b8fd86
Swords <33922797+SleepySwords@users.noreply.github.com>
2021-06-10 01:39:39 +1000

    Fixes removal of comment section
    This was able to be fixed as the youtube home page only updates the primary under `ytd-two-column-browse-results-renderer`, while the one under videos updates the primary under a div with the id columns.

--- removalScript.js
+++ removalScript.js
@@ -5,7 +5,7 @@ if(window.location.href.split("//")[1].split("/")[0] == "www.youtube.com") {
 		let items = document.getElementById("items")
 		items.parentNode.removeChild(items)
 	} else if(window.location.href.split("?")[0].split("/")[3] == "") {
-		let yt = document.getElementById("primary")
-		yt.parentNode.removeChild(yt)
+		let yt = document.getElementsByTagName("ytd-two-column-browse-results-renderer")
+		yt.removeChild("primary")
 	}
 }
