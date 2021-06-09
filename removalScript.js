f8519ef22c71b994cddbc4bb418509be39f94505
XcodeBoi <runningzoomcat@gmail.com>
2021-06-10 01:30:26 +1000

    add url checking; rework page selection;

--- removalScript.js
+++ removalScript.js
@@ -1,12 +1,11 @@
 console.log("exectuing...")
-console.log(window.location.href)
 
-if(window.location.href.split("?")[0].split("/")[3] != "watch") {
-	let yt = document.getElementById("primary")
-	yt.parentNode.removeChild(yt)
-} else {
-	let items = document.getElementById("items")
-	items.parentNode.removeChild(items)
+if(window.location.href.split("//")[1].split("/")[0] == "www.youtube.com") {
+	if(window.location.href.split("?")[0].split("/")[3] == "watch") {
+		let items = document.getElementById("items")
+		items.parentNode.removeChild(items)
+	} else if(window.location.href.split("?")[0].split("/")[3] == "") {
+		let yt = document.getElementById("primary")
+		yt.parentNode.removeChild(yt)
+	}
 }
-
-console.log("sucess")
