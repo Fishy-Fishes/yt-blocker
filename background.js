7429f278e841f881dab07643a03a03cb4b8cef23
XcodeBoi <runningzoomcat@gmail.com>
2021-06-10 11:26:54 +1000

    add comments; rework way element is removed home page;

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
+// run script when you move around the page and when the page is initally loaded
 chrome.webNavigation.onDOMContentLoaded.addListener(callRemoveScript);
 chrome.webNavigation.onHistoryStateUpdated.addListener(callRemoveScript);
 
 function callRemoveScript(details) {
+	// inject script on active tab
 	chrome.scripting.executeScript({
 		target: {
 			tabId: details.tabId
